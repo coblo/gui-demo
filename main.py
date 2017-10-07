e4ece13c333b666c3a11c241901ca08406da07fb
Titusz Pan <titusz.pan@gmail.com>
2017-10-07 22:50:23 +0200

    Show exceptions in console

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 
 """Main"""
 import sys
+import traceback
 
 from PyQt5.QtWidgets import QApplication
 
 from ui.ui_main import Ui_Main
 
 if __name__ == '__main__':
+    sys.excepthook = traceback.print_exception
     app = QApplication(sys.argv)
     ex = Ui_Main()
     sys.exit(app.exec_())

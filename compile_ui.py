80d8b1a0f8c4fa7c87143a238c704e786b3ab6b6
Titusz Pan <titusz.pan@gmail.com>
2017-10-09 16:22:10 +0200

    Fix compile ui to run from other modules

--- compile_ui.py
+++ compile_ui.py
@@ -6,8 +6,11 @@ import os
 from PyQt5.uic import compileUi
 
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
 def ui():
-    for uifile in glob("../app/ui/*.ui"):
+    for uifile in glob(os.path.join(HERE, "app/ui/*.ui")):
         pyfile = os.path.splitext(uifile)[0] + ".py"
         print(uifile)
         pyfile = open(pyfile, "wt", encoding="utf-8")
@@ -16,7 +19,7 @@ def ui():
 
 
 def resource():
-    for resfile in glob("../app/resources/*.qrc"):
+    for resfile in glob(os.path.join(HERE, "app/resources/*.qrc")):
         pyfile = os.path.splitext(resfile)[0] + "_rc.py"
         print(resfile)
         call("pyrcc5 -o {} {}".format(pyfile, resfile), shell=True)

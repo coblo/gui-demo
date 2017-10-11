b90ee09acce32f46a24f75c7982898c15a70acba
Titusz Pan <titusz.pan@gmail.com>
2017-10-11 11:52:28 +0200

    Add mining reward description to transaction parser

--- app/experimental/transaction_history.py
+++ app/experimental/transaction_history.py
@@ -4,20 +4,23 @@ txs = client.listwallettransactions(10000000)
 
 balance = 0
 for tx in txs['result']:
-    txid = tx['txid']
-    dt = datetime.fromtimestamp(tx['time'])
-    description = tx.get('comment', '')
-    perm = tx['permissions']
-    if perm:
-        description = 'Skills grant/revoke'
+    if tx['valid']:
+        txid = tx['txid']
+        dt = datetime.fromtimestamp(tx['time'])
+        description = tx.get('comment', '')
+        perm = tx['permissions']
+        if perm:
+            description = 'Skills grant/revoke'
 
-    items = tx['items']
-    if items:
-        first_item_type = items[0].get('type')
-        if first_item_type == 'stream':
-            description = 'Stream publishing'
+        items = tx['items']
+        if items:
+            first_item_type = items[0].get('type')
+            if first_item_type == 'stream':
+                description = 'Stream publishing'
+        if tx.get('generated'):
+            description = 'Mining reward'
 
-    amount = tx['balance']['amount']
-    balance += amount
-    perm = tx['permissions']
-    print(txid, dt, description, amount, balance)
+        amount = tx['balance']['amount']
+        balance += amount
+        perm = tx['permissions']
+        print(txid, dt, description, amount, balance)

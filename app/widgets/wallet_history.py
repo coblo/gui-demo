35fbecee5b30387be16039a3a45c6d3b49b98772
Patricia Schinke <schinke@creations.de>
2017-10-13 17:32:31 +0200

    Color unconfirmed transactions red

--- app/widgets/wallet_history.py
+++ app/widgets/wallet_history.py
@@ -1,6 +1,7 @@
 import operator
 
 from PyQt5.QtCore import QAbstractTableModel, QVariant, Qt
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QHeaderView, QWidget
 
 from app.ui.wallet_history import Ui_widget_wallet_history
@@ -15,23 +16,25 @@ class WalletHistory(QWidget, Ui_widget_wallet_history):
 
     def on_transactions_changed(self, transactions):
         data = [transaction['data'] for transaction in transactions]
-        table_model = TransactionHistoryTableModel(['Date', 'Description', 'Amount', 'Balance'], data)
+        unconfirmed = [index for index, transaction in enumerate(transactions) if not transaction['confirmed']]
+        table_model = TransactionHistoryTableModel(['Date', 'Description', 'Amount', 'Balance'], transactions, unconfirmed=unconfirmed)
         self.table_wallet_history.setModel(table_model)
         header = self.table_wallet_history.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(1, QHeaderView.Stretch)
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
-        # todo: transaction red when not confirmed
         # todo: amount red or green
 
 
 class TransactionHistoryTableModel(QAbstractTableModel):
 
-    def __init__(self, header, data, parent=None):
+    def __init__(self, header, transactions, parent=None, unconfirmed=None):
         super().__init__()
         self.header = header
-        self.data = data
+        self.transactions = transactions
+        self.data = [transaction['data'] for transaction in transactions]
+        self.unconfirmed = unconfirmed
 
     def rowCount(self, parent=None, *args, **kwargs):
         return len(self.data)
@@ -51,6 +54,8 @@ class TransactionHistoryTableModel(QAbstractTableModel):
             return QVariant(self.arraydata[index.row()][index.column()])
         elif role == Qt.TextAlignmentRole and index.column() != 1:
             return QVariant(Qt.AlignRight | Qt.AlignVCenter)
+        elif role == Qt.ForegroundRole and index.row() in self.unconfirmed:
+            return QVariant(QColor(Qt.red))
         return None
 
     def sort(self, p_int, order=None):
@@ -58,4 +63,5 @@ class TransactionHistoryTableModel(QAbstractTableModel):
         self.data = sorted(self.data, key=operator.itemgetter(p_int))
         if order == Qt.DescendingOrder:
             self.data.reverse()
+        self.unconfirmed = [self.data.index(transaction['data']) for transaction in self.transactions if not transaction['confirmed']]
         self.layoutChanged.emit()

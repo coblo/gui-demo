d175d6de96c8a4464f5af2459c11b9051bd4da86
Titusz Pan <titusz.pan@gmail.com>
2017-10-14 16:11:55 +0200

    History: refactor to not recreate model on every update

--- app/widgets/wallet_history.py
+++ app/widgets/wallet_history.py
@@ -10,11 +10,8 @@ class WalletHistory(QWidget, Ui_widget_wallet_history):
         super().__init__(parent)
         self.setupUi(self)
         self.updater = parent.updater
-        self.updater.transactions_changed.connect(self.on_transactions_changed)
-
-    def on_transactions_changed(self, transactions):
-        table_model = TransactionHistoryTableModel(['Date', 'Description', 'Amount', 'Balance'], transactions)
-        self.table_wallet_history.setModel(table_model)
+        self.table_model = TransactionHistoryTableModel(self)
+        self.table_wallet_history.setModel(self.table_model)
         header = self.table_wallet_history.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(1, QHeaderView.Stretch)
@@ -24,18 +21,20 @@ class WalletHistory(QWidget, Ui_widget_wallet_history):
 
 class TransactionHistoryTableModel(QAbstractTableModel):
 
-    def __init__(self, header, transactions, parent=None):
+    def __init__(self, parent=None):
         super().__init__()
-        self.header = header
-        self.transactions = transactions
-        self.data = [transaction['data'] for transaction in transactions]
-        self.unconfirmed = [index for index, transaction in enumerate(transactions) if not transaction['confirmed']]
+        self.updater = parent.updater
+        self.updater.transactions_changed.connect(self.on_transactions_changed)
+        self.header = ['Date', 'Description', 'Amount', 'Balance']
+        self.transactions = []
+        self.data = []
+        self.unconfirmed = []
 
     def rowCount(self, parent=None, *args, **kwargs):
         return len(self.data)
 
     def columnCount(self, parent=None, *args, **kwargs):
-        return len(self.data[0])
+        return len(self.header)
 
     def headerData(self, col, orientation, role=Qt.DisplayRole):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
@@ -66,3 +65,10 @@ class TransactionHistoryTableModel(QAbstractTableModel):
             self.data = sorted(self.data, key=lambda x: x[p_int], reverse=(order == Qt.DescendingOrder))
         self.unconfirmed = [self.data.index(transaction['data']) for transaction in self.transactions if not transaction['confirmed']]
         self.layoutChanged.emit()
+
+    def on_transactions_changed(self, transactions):
+        self.beginResetModel()
+        self.transactions = transactions
+        self.data = [transaction['data'] for transaction in self.transactions]
+        self.unconfirmed = [index for index, transaction in enumerate(self.transactions) if not transaction['confirmed']]
+        self.endResetModel()

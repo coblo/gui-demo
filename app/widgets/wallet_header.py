c61e2e74c2b60021fe235303335083d3f9da1a69
Titusz Pan <titusz.pan@gmail.com>
2017-10-14 23:30:22 +0200

    Performance: Initialize wallet header from stored settings

--- app/widgets/wallet_header.py
+++ app/widgets/wallet_header.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 
+from app import settings
 from app.backend.updater import Updater
 from app.ui.wallet_header import Ui_widget_wallet_header
 
@@ -9,17 +10,31 @@ class WalletHeader(QtWidgets.QWidget, Ui_widget_wallet_header):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
+
+        # Recover stored settings
+        balance = settings.value('balance')
+        if balance:
+            self.label_wallet_balance.setText(self.format_balance(balance))
+        address = settings.value('address')
+        if address:
+            self.label_wallet_address.setText(address)
+
+        # Listen to balance/address updates
         self.updater = parent.updater
         self.updater.balance_changed.connect(self.on_balance_changed)
         self.updater.address_changed.connect(self.on_address_changed)
 
-    def on_balance_changed(self, balance):
+    def format_balance(self, balance):
         display = "{0:n}".format(balance.normalize()) if balance is not ' ' else balance
-        display += ' CHM'
-        self.label_wallet_balance.setText(display)
+        return display + ' CHM'
+
+    def on_balance_changed(self, balance):
+        self.label_wallet_balance.setText(self.format_balance(balance))
+        settings.setValue('balance', balance)
 
     def on_address_changed(self, address):
         self.label_wallet_address.setText(address)
+        settings.setValue('address', address)
 
 
 if __name__ == '__main__':

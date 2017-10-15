269d7e6d6612ac825c2b6a7a6ba118522ab28d6a
Titusz Pan <titusz.pan@gmail.com>
2017-10-15 22:02:20 +0200

    Add address validation to WalletSend widget

--- app/widgets/wallet_send.py
+++ app/widgets/wallet_send.py
@@ -1,4 +1,7 @@
+from PyQt5.QtGui import QValidator
 from PyQt5.QtWidgets import QWidget
+
+from app.tools.validators import AddressValidator
 from app.ui.wallet_send import Ui_widget_wallet_send
 
 
@@ -6,3 +9,21 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.edit_address.setValidator(AddressValidator())
+
+        self.edit_address.textChanged.connect(self.check_state)
+        self.edit_address.textChanged.emit(self.edit_address.text())
+
+    def check_state(self, *args, **kwargs):
+        sender = self.sender()
+        validator = sender.validator()
+        state = validator.validate(sender.text(), 0)[0]
+        if state == QValidator.Acceptable:
+            color = '#c4df9b'  # green
+        elif state == QValidator.Intermediate:
+            color = '#fff79a'  # yellow
+        else:
+            color = '#f6989d'  # red
+        sender.setStyleSheet('QLineEdit { background-color: %s }' % color)
+

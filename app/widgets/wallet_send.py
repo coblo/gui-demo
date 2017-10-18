36fd2d44a3ff4a0f17a4b80e4357101601457e37
Titusz Pan <titusz.pan@gmail.com>
2017-10-18 13:10:18 +0200

    Auto healing amount input :)

--- app/widgets/wallet_send.py
+++ app/widgets/wallet_send.py
@@ -1,5 +1,6 @@
-from PyQt5.QtGui import QValidator, QDoubleValidator
+from PyQt5.QtGui import QValidator
 from PyQt5.QtWidgets import QWidget
+from decimal import Decimal
 
 from app import settings
 from app.tools.validators import AddressValidator
@@ -8,20 +9,19 @@ from app.backend.api import Api
 
 
 class WalletSend(QWidget, Ui_widget_wallet_send):
+
+    amount_symbols = '0123456789.'
+
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
         self.api = Api()
-
-        self.amount_validator = QDoubleValidator()
-        balance = float(settings.value('balance', 0.00))
-        self.amount_validator.setRange(0.00000001, balance, 8)
+        self.balance = settings.value('balance', Decimal('0'))
 
         self.updater = parent.updater
         self.updater.balance_changed.connect(self.on_balance_changed)
 
-        self.edit_amount.setValidator(self.amount_validator)
-        self.edit_amount.textChanged.connect(self.check_state)
+        self.edit_amount.textChanged.connect(self.on_amount_edit)
         self.edit_amount.textChanged.emit(self.edit_amount.text())
 
         self.edit_address.setValidator(AddressValidator())
@@ -30,6 +30,29 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
 
         self.btn_send_cancel.clicked.connect(self.on_cancel_clicked)
         self.btn_send_send.clicked.connect(self.on_send_clicked)
+
+    def on_amount_edit(self, text):
+        if not text:
+            self.edit_amount.setStyleSheet('QLineEdit { background-color: #fff79a}')
+            return
+        clean = ''.join(c for c in text.replace(',', '.') if c in self.amount_symbols)
+        shifter = []
+        dot_found = False
+        for c in reversed(clean):
+            if c != ".":
+                shifter.append(c)
+            else:
+                if dot_found is False:
+                    shifter.append(c)
+                    dot_found = True
+        clean = ''.join(reversed(shifter))
+        self.edit_amount.setText(clean)
+
+        amount = Decimal(clean)
+        if amount > self.balance or abs(amount.as_tuple().exponent) > 8:
+            self.edit_amount.setStyleSheet('QLineEdit { background-color: #f6989d}')
+        else:
+            self.edit_amount.setStyleSheet('QLineEdit { background-color: #c4df9b}')
 
     def check_state(self, *args, **kwargs):
         sender = self.sender()
@@ -49,8 +72,11 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
         self.edit_description.clear()
 
     def on_send_clicked(self):
-        amount = float(self.edit_amount.text().replace(',', '.'))
-        success = self.api.send(address=self.edit_address.text(), amount=amount, description=self.edit_description.text())
+        success = self.api.send(
+            address=self.edit_address.text(),
+            amount=Decimal(self.edit_amount.text()),
+            description=self.edit_description.text()
+        )
         if success:
             self.edit_address.setText('')
             self.edit_amount.setText('')
@@ -58,10 +84,4 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
             self.updater.on_send()
 
     def on_balance_changed(self, balance):
-        if balance is None:
-            balance = float(settings.value('balance', 0.00))
-        else:
-            balance = float(settings.value('balance', balance))
-        self.amount_validator.setRange(0.00000001, balance, 8)
-
-
+        self.balance = balance

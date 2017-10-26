b578569c7d47465d55a4b95bd2c903c0599c7f60
Titusz Pan <titusz.pan@gmail.com>
2017-10-26 14:57:15 +0200

    new singe-ui-file prototype

---
+++ app/widgets/proto.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+
+from app.tools.runner import run_widget
+from app.ui.proto import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_grp_nav.setId(self.btn_nav_wallet, 0)
+        self.btn_grp_nav.setId(self.btn_nav_community, 1)
+        self.btn_grp_nav.setId(self.btn_nav_settings, 2)
+        self.centralwidget.setDisabled(False)
+
+
+if __name__ == '__main__':
+    run_widget(MainWindow)

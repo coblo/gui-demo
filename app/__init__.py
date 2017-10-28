cdf61cd3fb580aee6f5085762596a5fcf1a0b89a
Titusz Pan <titusz.pan@gmail.com>
2017-10-28 19:49:54 +0200

    Fix APP_DIR in non frozen state

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Global constants and pre-app initialization stuff."""
-import os
+from os.path import abspath, dirname, join
 import sys
 import appdirs
 
@@ -15,13 +15,13 @@ APP_VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
 
 
 if getattr(sys, "frozen", False):
-    APP_DIR = os.path.dirname(sys.executable)
+    APP_DIR = dirname(sys.executable)
 else:
-    APP_DIR = os.path.dirname(__file__)
+    APP_DIR = dirname(dirname(abspath(__file__)))
 
 DATA_DIR = appdirs.user_data_dir(APP_NAME, ORG_NAME)
 PROFILE_DB_FILENAME = 'profile.db'
-PROFILE_DB_FILEPATH = os.path.join(DATA_DIR, PROFILE_DB_FILENAME)
+PROFILE_DB_FILEPATH = join(DATA_DIR, PROFILE_DB_FILENAME)
 DEFAULT_PROFILE_NAME = 'default'
 DEFAULT_RPC_HOST = '127.0.0.1'
 DEFAULT_RPC_PORT = 8374

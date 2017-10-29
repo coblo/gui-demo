40b8db8c1718dca24c0d5bd321ca74dff20eee72
Titusz Pan <titusz.pan@gmail.com>
2017-10-30 00:03:57 +0100

    some more fields on transaction orm model

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -10,7 +10,16 @@ log = logging.getLogger(__name__)
 class Transaction(peewee.Model):
     """Wallet transactions"""
 
+    datetime = peewee.DateTimeField()
+    comment = peewee.CharField()
+    amount = peewee.DecimalField(max_digits=17, decimal_places=8)
+    balance = peewee.DecimalField(max_digits=17, decimal_places=8)
+    confirmations = peewee.SmallIntegerField()
     txid = peewee.CharField(primary_key=True)
 
     class Meta:
         database = data_db
+
+    def value_by_col(self, col):
+        fn = self._meta.sorted_field_names[col]
+        return getattr(self, fn)

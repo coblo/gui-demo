cbbaf9d09ff05aae0942ee44c63cca730c98059e
Titusz Pan <titusz.pan@gmail.com>
2017-10-30 00:24:17 +0100

    set name on signals object for eventual connect by name

--- app/signals.py
+++ app/signals.py
@@ -5,13 +5,24 @@ from PyQt5.QtCore import QObject, pyqtSignal, QProcess
 
 class Signals(QObject):
 
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setObjectName('sync')
+
     # Blockchain node status
     node_started = pyqtSignal()
     node_finished = pyqtSignal(int, QProcess.ExitStatus)
     node_error = pyqtSignal(QProcess.ProcessError)
 
-    # Blockchain sync
+    # rpc method syncs
+    getinfo = pyqtSignal(object)
+    getblockchaininfo = pyqtSignal(object)
+    getruntimeparams = pyqtSignal(object)
+
+    listwallettransactions = pyqtSignal()
+
     block_sync_changed = pyqtSignal(dict)
+    transactions_changed = pyqtSignal(list)
 
 
 signals = Signals()

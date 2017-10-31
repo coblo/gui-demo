f1cdfa0fa1aef1709ad52a112f882fa0ad7a09e5
Titusz Pan <titusz.pan@gmail.com>
2017-10-31 20:50:20 +0100

    wallet send: use latest balance at amount validation

--- app/widgets/wallet_send.py
+++ app/widgets/wallet_send.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QWidget
 from decimal import Decimal
 
 from app.backend.rpc import get_active_rpc_client
+from app.enums import SettingKey
 from app.tools.validators import AddressValidator
 from app.ui.wallet_send import Ui_widget_wallet_send
 from app.settings import settings
@@ -18,7 +19,6 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
-        self.balance = settings.value('balance', Decimal("0"))
 
         self.edit_amount.textChanged.connect(self.on_amount_edit)
 
@@ -51,7 +51,7 @@ class WalletSend(QWidget, Ui_widget_wallet_send):
             amount = Decimal(clean)
         except Exception as e:
             return
-        if amount > self.balance or abs(amount.as_tuple().exponent) > 8:
+        if amount > settings.value(SettingKey.balance.name, Decimal("0")) or abs(amount.as_tuple().exponent) > 8:
             self.edit_amount.setStyleSheet('QLineEdit { background-color: #f6989d}')
         else:
             self.edit_amount.setStyleSheet('QLineEdit { background-color: #c4df9b}')

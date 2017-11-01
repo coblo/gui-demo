345e1565c15dba64568470ba99957b48bec9debc
Titusz Pan <titusz.pan@gmail.com>
2017-11-02 00:35:14 +0100

    added query methods to address model

--- app/models/address.py
+++ app/models/address.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import peewee
+
 from app.models.db import data_db
 
 
@@ -8,13 +9,42 @@ log = logging.getLogger(__name__)
 
 
 class Address(peewee.Model):
-    """Addresses"""
+    """
+    TODO: use query annotations or denormalize fields
+    """
 
     address = peewee.CharField(primary_key=True)
     alias = peewee.CharField(unique=True, null=True)
 
+    class Meta:
+        database = data_db
+
     def __repr__(self):
         return 'Address(%s, %s)' % (self.address, self.alias)
 
-    class Meta:
-        database = data_db
+    def last_mined(self):
+        from app.models import Block
+        latest_block = self.mined_blocks.order_by(Block.time.desc()).first()
+        if latest_block:
+            return latest_block.time
+
+    def last_voted(self):
+        return 'TODO find out :)'
+
+    def num_validator_revokes(self):
+        from app.models import VotingRound, Permission
+        return VotingRound.select().where(
+            (VotingRound.address == self) &
+            (VotingRound.perm_type == Permission.MINE) &
+            (VotingRound.start_block == 0) &
+            (VotingRound.end_block == 0)
+        ).count()
+
+    def num_guardian_revokes(self):
+        from app.models import VotingRound, Permission
+        return VotingRound.select().where(
+            (VotingRound.address == self) &
+            (VotingRound.perm_type == Permission.ADMIN) &
+            (VotingRound.start_block == 0) &
+            (VotingRound.end_block == 0)
+        ).count()

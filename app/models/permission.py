1a3c66ee66717b580ddda4c120f0530e74be370a
Titusz Pan <titusz.pan@gmail.com>
2017-11-02 00:40:44 +0100

    helper methods and repr on Permission model

--- app/models/permission.py
+++ app/models/permission.py
@@ -12,6 +12,7 @@ log = logging.getLogger(__name__)
 class Permission(peewee.Model):
     """Address permissions"""
 
+    # TODO move model constants to app.enums
     ISSUE, CREATE, MINE, ADMIN = 'issue', 'create', 'mine', 'admin'
     PERM_TYPES = ISSUE, CREATE, MINE, ADMIN
     MAX_END_BLOCK = 4294967295
@@ -25,19 +26,32 @@ class Permission(peewee.Model):
         database = data_db
         primary_key = peewee.CompositeKey('address', 'perm_type')
 
+    def __repr__(self):
+        return "Permission(%s, %s, %s, %s)" % (
+            self.address_id[:4], self.perm_type, self.start_block, self.end_block
+        )
+
     @staticmethod
-    def num_validators():
+    def validators():
         return Permission.select().where(
             Permission.perm_type == Permission.MINE,
             Permission.start_block == 0,
             Permission.end_block == Permission.MAX_END_BLOCK,
-        ).count()
+        )
 
     @staticmethod
-    def num_guardians():
+    def guardians():
         return Permission.select().where(
             Permission.perm_type == Permission.ADMIN,
             Permission.start_block == 0,
             Permission.end_block == Permission.MAX_END_BLOCK,
-        ).count()
+        )
+
+    @staticmethod
+    def num_validators():
+        return Permission.validators().count()
+
+    @staticmethod
+    def num_guardians():
+        return Permission.guardians().count()
 

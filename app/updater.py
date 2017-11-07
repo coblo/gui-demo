9438b1a57b27402eee53ad9707244d370335c48d
Titusz Pan <titusz.pan@gmail.com>
2017-11-07 21:29:44 +0100

    only do full syncs if there is a new block or a new wallet transaction

--- app/updater.py
+++ app/updater.py
@@ -14,6 +14,17 @@ class Updater(QtCore.QThread):
 
     UPDATE_INTERVALL = 3
 
+    sync_funcs = (
+        sync.getinfo,
+        sync.getruntimeparams,
+        sync.getblockchaininfo,
+        sync.listblocks,
+        sync.listwallettransactions,
+        sync.listpermissions,
+        sync.liststreamitems_alias,
+        sync.getblock,
+    )
+
     def __init__(self, parent=None):
         super().__init__(parent)
         log.debug('init updater')
@@ -27,24 +38,30 @@ class Updater(QtCore.QThread):
         return get_active_rpc_client()
 
     def run(self):
+
+        synced_blockhash = ''
+        synced_txid = ''
+
         while True:
 
-            sync_funcs = (
-                sync.getinfo,
-                sync.getruntimeparams,
-                sync.getblockchaininfo,
-                sync.listblocks,
-                sync.listwallettransactions,
-                sync.listpermissions,
-                sync.liststreamitems_alias,
-                sync.getblock,
-            )
+            log.debug('check for new block or new local wallet updates')
+            node_block_hash = self.client.getbestblockhash()['result']
+            try:
+                node_txid = self.client.listwallettransactions(1)['result'][0]['txid']
+            except KeyError:
+                node_txid = ''
 
-            for sync_func in sync_funcs:
-                try:
-                    log.debug('updating %s' % sync_func.__name__)
-                    sync_func()
-                except Exception as e:
-                    log.exception(e)
+            if node_block_hash != synced_blockhash or node_txid != synced_txid:
+                log.debug('starting full sync round')
 
-            time.sleep(self.UPDATE_INTERVALL)
+                for sync_func in self.sync_funcs:
+                    try:
+                        log.debug('updating %s' % sync_func.__name__)
+                        sync_func()
+                    except Exception as e:
+                        log.exception(e)
+
+                synced_blockhash = node_block_hash
+                synced_txid = node_txid
+
+            self.sleep(self.UPDATE_INTERVALL)

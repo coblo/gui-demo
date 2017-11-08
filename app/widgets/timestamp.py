508ecd6722803a6698b76af11cdcb4c9e44fd410
Titusz Pan <titusz.pan@gmail.com>
2017-11-08 23:25:07 +0100

    working hashing with progress in background thread

--- app/widgets/timestamp.py
+++ app/widgets/timestamp.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-from PyQt5.QtCore import QMimeData, QUrl, pyqtSlot, QObject, QEvent
+from PyQt5.QtCore import QMimeData, QUrl, pyqtSlot, QObject, QEvent, pyqtSignal, QThread
 from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QDragLeaveEvent
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QFileDialog
 from app.ui.timestamp import Ui_WidgetTimestamping
 from hashlib import sha256
 
@@ -11,17 +11,71 @@ from hashlib import sha256
 log = logging.getLogger(__name__)
 
 
+class Hasher(QThread):
+
+    #: emits num bytes processed
+    hashing_progress = pyqtSignal(int)
+
+    def __init__(self, file_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_path = file_path
+        self.result = None
+
+    def run(self):
+        hasher = sha256()
+        progress = 0
+        with open(self.file_path, 'rb') as f:
+            while True:
+                chunk = f.read(1024)
+                if not chunk:
+                    break
+                else:
+                    hasher.update(chunk)
+                    progress += len(chunk)
+                    self.hashing_progress.emit(progress)
+        self.result = hasher.hexdigest()
+
+
 class WidgetTimestamping(QWidget, Ui_WidgetTimestamping):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.setupUi(self)
         self.reset()
+        self.hash_thread = None
 
+        # Intercept drag & drop events from button
         self.button_dropzone.installEventFilter(self)
 
         # Connections
+        self.button_dropzone.clicked.connect(self.file_select_dialog)
         self.button_reset.clicked.connect(self.reset)
+
+    def process_file(self, file_path):
+        log.debug('proccess file %s' % file_path)
+        self.button_dropzone.setText("Current File: %s" % os.path.basename(file_path))
+        self.gbox_processing_status.setEnabled(True)
+        self.progress_bar.setMaximum(os.path.getsize(file_path))
+        self.progress_bar.setValue(0)
+        self.progress_bar.show()
+        self.label_processing_status.setText('Calculating fingerprint ...')
+        self.button_dropzone.setStyleSheet('background-color: #0183ea; color: white;')
+        self.hash_thread = Hasher(file_path)
+        self.hash_thread.hashing_progress.connect(self.progress_bar.setValue)
+        self.hash_thread.finished.connect(self.hash_thread_finished)
+        self.hash_thread.start()
+
+    @pyqtSlot()
+    def hash_thread_finished(self):
+        log.debug('hashing finished with: %s' % self.hash_thread.result)
+
+
+    @pyqtSlot()
+    def file_select_dialog(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Open file')
+        if file_path:
+            self.process_file(file_path)
 
     def eventFilter(self, obj: QObject, event: QEvent):
         if event.type() == QEvent.DragEnter:
@@ -32,6 +86,7 @@ class WidgetTimestamping(QWidget, Ui_WidgetTimestamping):
             self.on_drag_leave(obj, event)
         elif event.type() == QEvent.Drop:
             log.debug('Drop')
+            self.on_drop(obj, event)
         return QWidget.eventFilter(self, obj, event)
 
     def on_drag_enter(self, obj: QObject, event: QDragEnterEvent):
@@ -46,18 +101,23 @@ class WidgetTimestamping(QWidget, Ui_WidgetTimestamping):
 
             if not url.isValid():
                 return self.reject_drag(event, 'Invalid URL. Try again!')
-            elif not url.isLocalFile():
+            if not url.isLocalFile():
                 return self.reject_drag(event, 'Only local files are supported. Try again!')
-            elif os.path.isdir(url.toLocalFile()):
+            if os.path.isdir(url.toLocalFile()):
                 return self.reject_drag(event, 'Directories not supported. Try again!')
-            else:
-                event.accept()
+
+            event.accept()
+            self.button_dropzone.setStyleSheet('background-color: green; color: white;')
+            self.button_dropzone.setText('Just drop it :)')
 
     def on_drag_leave(self, obj: QObject, event: QDragLeaveEvent):
         self.button_dropzone.setText('Drop your file here or click to choose.')
         self.button_dropzone.setStyleSheet('background-color: #0183ea; color: white;')
         self.button_dropzone.style().polish(self.button_dropzone)
-        event.accept()
+
+    def on_drop(self, obj: QObject, event: QDropEvent):
+        file_path = event.mimeData().urls()[0].toLocalFile()
+        self.process_file(file_path)
 
     def reject_drag(self, event, message):
         self.button_dropzone.setText(message)
@@ -73,42 +133,14 @@ class WidgetTimestamping(QWidget, Ui_WidgetTimestamping):
         self.button_dropzone.setStyleSheet('background-color: #0183ea; color: white;')
         self.button_dropzone.setText('Drop your file here or click to choose.')
 
-    def set_file(self, file_path):
-        self.file = file_path
-        text = "Current File: %s" % os.path.basename(self.file)
-        self.lbl_dropzone.setText(text)
-        self.progress_bar.show()
-        self.calc_sha256()
-
-    def calc_sha256(self):
-        hasher = sha256()
-        progress = 0
-        with open(self.file, 'rb') as f:
-            while True:
-                chunk = f.read(64)
-                if not chunk:
-                    break
-                else:
-                    hasher.update(chunk)
-                    progress += len(chunk)
-                    self.progress_bar.setValue(progress)
-        self.lbl_dropzone.setText(hasher.hexdigest())
-
-
-    def dropEvent(self, event: QDropEvent):
-        file_path = event.mimeData().urls()[0].toLocalFile()
-        self.set_file(file_path)
-        event.accept()
-        log.debug('TODO: hash and register: %s' % file_path)
-
 
 if __name__ == '__main__':
     import sys
-    from PyQt5 import QtWidgets, QtCore
-    from app import helpers
-    helpers.init_logging()
-    app = QtWidgets.QApplication(sys.argv)
-    app.setStyle('fusion')
+    from PyQt5 import QtWidgets
+    import app
+    app.init()
+    wrapper = QtWidgets.QApplication(sys.argv)
+    wrapper.setStyle('fusion')
     dialog = WidgetTimestamping()
     dialog.show()
-    sys.exit(app.exec())
+    sys.exit(wrapper.exec())

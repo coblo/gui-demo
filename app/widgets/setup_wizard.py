98b3aabe6c2821f8ed14b65384d2e4b3525eb19a
Titusz Pan <titusz.pan@gmail.com>
2017-11-11 21:18:03 +0100

    Implemented connect to existing node.

--- app/widgets/setup_wizard.py
+++ app/widgets/setup_wizard.py
@@ -3,6 +3,8 @@ import logging
 
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
+
+from app.backend.rpc import RpcClient
 from app.ui.setup_wizard import Ui_SetupWizard
 from PyQt5.QtWidgets import QWizard
 from app.ui import resources_rc
@@ -26,6 +28,9 @@ class SetupWizard(QWizard, Ui_SetupWizard):
         self.setPixmap(QWizard.LogoPixmap, QPixmap(':/images/resources/wizard_logo.png'))
         self.setPixmap(QWizard.BannerPixmap, QPixmap(':/images/resources/wizard_banner.png'))
 
+        # Wizard state
+        self._connection_tested = False
+
         # Global connections
         self.currentIdChanged.connect(self.current_id_changed)
 
@@ -37,7 +42,12 @@ class SetupWizard(QWizard, Ui_SetupWizard):
         # Page 2 Choose Installation Mode
         self.button_group_choose_mode.buttonClicked.connect(self.next)
 
-        # Page 3 Choose Account Create or Import
+        # Page 3 Connect to existing node
+        self.page3_connect.isComplete = self.page3_connect_is_complete
+        self.button_test_connection.clicked.connect(self.test_connection)
+        self.button_reset_connection_form.clicked.connect(self.reset_connection_form)
+
+        # Page 4 Choose Account Create or Import
         self.button_group_choose_account.buttonClicked.connect(self.next)
 
 
@@ -51,10 +61,49 @@ class SetupWizard(QWizard, Ui_SetupWizard):
     def page1_license_is_complete(self):
         return self.radio_accept_license.isChecked()
 
+    @pyqtSlot()
+    def page3_connect_is_complete(self):
+        # Verify connection to rpc host
+        log.debug('Page 3 completion check request')
+        return self._connection_tested
+
+    @pyqtSlot()
+    def test_connection(self):
+        client = RpcClient(
+            host=self.edit_rpc_host.text(),
+            port=self.edit_rpc_port.text(),
+            user=self.edit_rpc_user.text(),
+            pwd=self.edit_rpc_password.text(),
+            use_ssl=self.cbox_use_ssl.isChecked(),
+        )
+        try:
+            response = client.getinfo()
+            assert response['error'] is None
+        except Exception as e:
+            self.label_test_connection.setText(str(e))
+            return
+
+        msg = 'Successfully connected to %s' % response['result']['description']
+        self.label_test_connection.setText(msg)
+        self.button_test_connection.setDisabled(True)
+        self.gbox_connect.setEnabled(False)
+        self._connection_tested = True
+        self.page3_connect.completeChanged.emit()
+
+    @pyqtSlot()
+    def reset_connection_form(self):
+        for wgt in self.gbox_connect.children():
+            if isinstance(wgt, QtWidgets.QLineEdit):
+                wgt.clear()
+            if isinstance(wgt, QtWidgets.QCheckBox):
+                wgt.setChecked(False)
+        self.label_test_connection.setText('Please test the connection to proceed.')
+        self.button_test_connection.setEnabled(True)
+        self._connection_tested = False
+        self.gbox_connect.setEnabled(True)
+        self.page3_connect.completeChanged.emit()
+
     def page2_next_id(self):
-        log.debug('PAGE NEXT ID CALLED')
-        selection = self.button_group_choose_mode.checkedId()
-        log.debug('Selected button %s' % selection)
         if self.button_connect_node.isChecked():
             return self.P3_CONNECT
         elif self.button_setup_node.isChecked():

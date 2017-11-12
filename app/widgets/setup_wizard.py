9a4a4be66f483babbb020b8fb6107ad2ddb1482a
Titusz Pan <titusz.pan@gmail.com>
2017-11-12 18:21:21 +0100

    support for mnemonic create and import

--- app/widgets/setup_wizard.py
+++ app/widgets/setup_wizard.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+from mnemonic import Mnemonic
 
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
@@ -32,6 +33,7 @@ class SetupWizard(QWizard, Ui_SetupWizard):
         # Wizard state
         self._connection_tested = False
         self._sync_ready = False
+        self._mnemonic = None
 
         # Global connections
         self.currentIdChanged.connect(self.current_id_changed)
@@ -46,11 +48,21 @@ class SetupWizard(QWizard, Ui_SetupWizard):
 
         # Page 3 Connect to existing node
         self.page3_connect.isComplete = self.page3_connect_is_complete
+        self.page3_connect.cleanupPage = self.reset_connection_form
         self.button_test_connection.clicked.connect(self.test_connection)
         self.button_reset_connection_form.clicked.connect(self.reset_connection_form)
 
         # Page 4 Choose Account Create or Import
         self.button_group_choose_account.buttonClicked.connect(self.next)
+
+        # Page 5 Import Existing Account
+        self.page5_import_account.isComplete = self.page5_import_account_is_complete
+        self.edit_seed.textChanged.connect(self.page5_import_account.completeChanged)
+
+        # Page 6 Create Account
+        self.button_generate_mnemonic.clicked.connect(self.generate_mnemonic)
+        self.page6_create_account.isComplete = self.page6_create_account_is_complete
+        self.page6_create_account.cleanupPage = self.page6_create_account_cleanup
 
         # Page 7 Initial Sync
         self.page7_sync.isComplete = self.page7_sync_is_complete
@@ -62,16 +74,54 @@ class SetupWizard(QWizard, Ui_SetupWizard):
             self.button(QWizard.NextButton).hide()
 
     @pyqtSlot()
+    def generate_mnemonic(self):
+        if self._mnemonic is None:
+            self._mnemonic = Mnemonic('english').generate(256)
+            self.label_new_seed_info.setText(
+                'This is your mnemonic. Please make sure to create a safe backup of this phrase before you proceed!'
+            )
+            self.label_new_seed_info.setStyleSheet('color: red;')
+            self.edit_new_seed.setPlainText(self._mnemonic)
+            self.button_generate_mnemonic.hide()
+            self.page6_create_account.completeChanged.emit()
+
     def page1_license_is_complete(self):
         return self.radio_accept_license.isChecked()
 
-    @pyqtSlot()
     def page3_connect_is_complete(self):
         # Verify connection to rpc host
         log.debug('Page 3 completion check request')
         return self._connection_tested
 
-    @pyqtSlot()
+    def page5_import_account_is_complete(self):
+        words = self.edit_seed.toPlainText()
+
+        if not words:
+            self.edit_seed.setStyleSheet('background-color: white;')
+            return False
+
+        validator = Mnemonic('english')
+        if validator.check(words):
+            self.edit_seed.setStyleSheet('background-color: #c4df9b;')
+            return True
+        else:
+            self.edit_seed.setStyleSheet('background-color: #fff79a;')
+            return False
+
+    def page6_create_account_is_complete(self):
+        return self._mnemonic is not None
+
+    def page6_create_account_cleanup(self):
+        self._mnemonic = None
+        self.label_new_seed_info.setStyleSheet('color: black;')
+        self.label_new_seed_info.setText(
+            'You account is made from 24 words that are randomly generated. '
+            'This is called a mnemonic. '
+            'Click on "Create New Account" to generate your mnemonic!'
+        )
+        self.edit_new_seed.setPlainText('')
+        self.button_generate_mnemonic.show()
+
     def page7_sync_is_complete(self):
         return self._sync_ready
 
@@ -135,11 +185,11 @@ class SetupWizard(QWizard, Ui_SetupWizard):
 
 if __name__ == '__main__':
     import sys
-    from PyQt5 import QtWidgets
+    from PyQt5 import QtWidgets, QtCore
     from app.helpers import init_logging
-    # import app
-    # app.init()
-    init_logging()
+    import app
+    app.init()
+    # init_logging()
     wrapper = QtWidgets.QApplication(sys.argv)
     wrapper.setStyle('fusion')
     wizard = SetupWizard()

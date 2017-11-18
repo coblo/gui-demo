41a9078027132570a698be47eeb9c8d32ba67ff8
Titusz Pan <titusz.pan@gmail.com>
2017-11-18 15:06:55 +0100

    update runchain example but

--- runchain.example.bat
+++ runchain.example.bat
@@ -1,10 +1,11 @@
 # Example Windows batch file to launch blockchain node
 # Update path to chain exexutable and connection string and rename this file to runchain.bat
 
-C:\path_to\multichaind.exe chain@host:port ^
+C:\path_to\multichaind.exe coblo@89.163.206.83:8375 ^
 -printtoconsole ^
 -server=1 ^
 -autosubscribe=assets,streams ^
+-autocombineminconf=4294967294 ^
 -rpcbind=127.0.0.1 ^
 -rpcallowip=127.0.0.1 ^
--rpcport=8374
+-rpcport=8374 ^

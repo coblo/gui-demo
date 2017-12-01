a44918826575bb47528813beccf0916fe5695aed
André Hänsel <haensel@creations.de>
2017-12-01 08:43:38 +0100

    Use constants for organization and app name

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,8 @@
 from PyQt5.QtCore import QSettings
 
+import app
+
 
 class Settings(QSettings):
     def __init__(self):
-        super().__init__('Content-Blockchain', 'Coblo')
+        super().__init__(app.ORG_NAME, app.APP_NAME)

733d8e93d2bcca7c8343041970691c1e7b68b79f
Patricia Schinke <schinke@creations.de>
2018-01-25 19:02:17 +0100

    Limit wallet history
    Fixes #81

--- app/widgets/wallet_history.py
+++ app/widgets/wallet_history.py
@@ -54,9 +54,13 @@ class TransactionTableView(QTableView):
         self.create_table_buttons()
 
     def create_table_buttons(self):
+        from app.models.db import data_session_scope
         self.setItemDelegateForColumn(5, ButtonDelegate(self))
-        for row in range(0, self.table_model.rowCount()):
+        for row in range(0, self.table_model.rowCount() - 1):
             self.openPersistentEditor(self.table_model.index(row, 5))
+        if not self.table_model.all_transactions_shown:
+            self.setItemDelegateForColumn(2, ButtonShowDelegate(self))
+            self.openPersistentEditor(self.table_model.index(self.table_model.rowCount() - 1, 2))
         # TODO find a better way to fix button sizes collapsing on update
         w, h = self.size().width(), self.size().height()
         self.resize(w + 1, h)
@@ -82,8 +86,8 @@ class ButtonDelegate(QStyledItemDelegate):
             "QPushButton {margin: 8 4 8 4; border: none;}")
         btn.setObjectName(table_entry.txid)
         btn.clicked.connect(self.on_info_clicked)
-        btn.setCursor(QCursor(Qt.PointingHandCursor))
         return btn
+
 
     def on_info_clicked(self):
         sender = self.sender()
@@ -91,6 +95,22 @@ class ButtonDelegate(QStyledItemDelegate):
         link = 'https://explorer.content-blockchain.org/Content%20Blockchain%20Project%20(Testnet)/tx/'
         link += txid
         webbrowser.open(link)
+
+class ButtonShowDelegate(QStyledItemDelegate):
+    def __init__(self, parent):
+        QStyledItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, idx):
+        btn = QPushButton("Show more transactions", parent)
+        btn.setStyleSheet("QPushButton {background-color: #0183ea; margin: 8 4 8 4; color: white; width: 45px}")
+        btn.setFont(QFont("Roboto", 12))
+        btn.setCursor(QCursor(Qt.PointingHandCursor))
+        btn.clicked.connect(self.on_show_more_clicked)
+        return btn
+
+    def on_show_more_clicked(self):
+        self.parent().table_model.limit += 100
+        self.parent().table_model.refreshData()
 
 class TransactionHistoryTableModel(QAbstractTableModel):
 
@@ -129,6 +149,8 @@ class TransactionHistoryTableModel(QAbstractTableModel):
         self.txs = None
         self.sort_index = self.DATETIME
         self.sort_order = Qt.AscendingOrder
+        self.limit = 100
+        self.all_transactions_shown = False
         self.refreshData()
         signals.wallet_transactions_changed.connect(self.refreshData)
 
@@ -136,7 +158,7 @@ class TransactionHistoryTableModel(QAbstractTableModel):
         self.txs = self.txs + [data]
 
     def rowCount(self, parent=None, *args, **kwargs):
-        return len(self.txs)
+        return len(self.txs) if self.all_transactions_shown else len(self.txs) + 1
 
     def columnCount(self, parent=None, *args, **kwargs):
         return len(self.header)
@@ -148,45 +170,46 @@ class TransactionHistoryTableModel(QAbstractTableModel):
 
     def data(self, index, role=Qt.DisplayRole):
         row, col = index.row(), index.column()
-        tx = self.txs[row]
-        if role == Qt.DisplayRole:
-            if col == self.DATETIME:
-                return 'unconfirmed' if tx[col] is None else "{}".format(tx[col])
-            if col == self.AMOUNT:
-                amount = tx[col]
-                if amount == 0:
-                    amount = 0
-                display = "{0:n}".format(amount)
-                return '+' + display if amount > 0 else display
-            if col == self.BALANCE:
-                if tx[col] is None:
-                    return '-'
-                normalized = tx[col].quantize(Decimal('.01'), rounding=ROUND_DOWN)
-                display = "{0:n}".format(normalized)
-                return display
-            if col == self.TXTYPE:
-                return None
-            if col == self.INFO:
-                return None
-            return tx[col]
+        if row < len(self.txs):
+            tx = self.txs[row]
+            if role == Qt.DisplayRole:
+                if col == self.DATETIME:
+                    return 'unconfirmed' if tx[col] is None else "{}".format(tx[col])
+                if col == self.AMOUNT:
+                    amount = tx[col]
+                    if amount == 0:
+                        amount = 0
+                    display = "{0:n}".format(amount)
+                    return '+' + display if amount > 0 else display
+                if col == self.BALANCE:
+                    if tx[col] is None:
+                        return '-'
+                    normalized = tx[col].quantize(Decimal('.01'), rounding=ROUND_DOWN)
+                    display = "{0:n}".format(normalized)
+                    return display
+                if col == self.TXTYPE:
+                    return None
+                if col == self.INFO:
+                    return None
+                return tx[col]
 
-        if role == Qt.DecorationRole and col == self.TXTYPE and tx[col] in self.transaction_types:
-            return self.transaction_type_to_icon[tx[col]]
-        if role == Qt.ToolTipRole:
-            if col == self.BALANCE:
-                return '_' if tx[col] is None else "{0:n}".format(tx[col])
-            elif col == self.TXTYPE and tx[col] in self.transaction_types:
-                return self.transaction_types[tx[col]]
-            else:
-                return None
-        elif role == Qt.TextAlignmentRole and col not in (self.COMMENT, self.DATETIME):
-            return QVariant(Qt.AlignRight | Qt.AlignVCenter)
-        elif role == Qt.ForegroundRole:
-            if col == self.AMOUNT and tx[col] < 0:
-                return QVariant(QColor(Qt.red))
-        elif role == Qt.FontRole and col == self.AMOUNT:
-            font = QFont("RobotoCondensed-Light", 9)
-            return QVariant(font)
+            if role == Qt.DecorationRole and col == self.TXTYPE and tx[col] in self.transaction_types:
+                return self.transaction_type_to_icon[tx[col]]
+            if role == Qt.ToolTipRole:
+                if col == self.BALANCE:
+                    return '_' if tx[col] is None else "{0:n}".format(tx[col])
+                elif col == self.TXTYPE and tx[col] in self.transaction_types:
+                    return self.transaction_types[tx[col]]
+                else:
+                    return None
+            elif role == Qt.TextAlignmentRole and col not in (self.COMMENT, self.DATETIME):
+                return QVariant(Qt.AlignRight | Qt.AlignVCenter)
+            elif role == Qt.ForegroundRole:
+                if col == self.AMOUNT and tx[col] < 0:
+                    return QVariant(QColor(Qt.red))
+            elif role == Qt.FontRole and col == self.AMOUNT:
+                font = QFont("RobotoCondensed-Light", 9)
+                return QVariant(font)
         return None
 
     def sort(self, p_int, order=None):
@@ -204,8 +227,13 @@ class TransactionHistoryTableModel(QAbstractTableModel):
         from app.models.db import data_session_scope
         self.beginResetModel()
         with data_session_scope() as session:
-            self.txs = WalletTransaction.get_wallet_history(session)
+            self.txs = WalletTransaction.get_wallet_history(session, self.limit)
         self.endResetModel()
         self.sort(self.sort_index, self.sort_order)
         if hasattr(self.parent, "table_model"):
             self.parent.create_table_buttons()
+        with data_session_scope() as session:
+            if session.query(WalletTransaction).count() == len(self.txs) and not self.all_transactions_shown:
+                self.all_transactions_shown = True
+                self.removeRow(self.rowCount())
+                self.refreshData()
